2b7ff495abf92f025aaf028a077226efa0783b49
Midweekvermin <113259156+Midweekvermin@users.noreply.github.com>
2023-05-20 12:55:14 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,100 @@
+import React from 'react';
+import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+
+
+const Clarify = (imageUrl) => {
+  // Your PAT (Personal Access Token) can be found in the portal under Authentification
+  const PAT = '30a7e085f91e49249b88d13c26db2ac9';
+  // Specify the correct user_id/app_id pairings
+  // Since you're making inferences outside your app's scope
+  const USER_ID = 'midweekvermin';       
+  const APP_ID = 'test';
+  // Change these to whatever model and image URL you want to use
+  const MODEL_ID = 'face-detection';
+  
+  const IMAGE_URL = imageUrl;
+
+  ///////////////////////////////////////////////////////////////////////////////////
+  // YOU DO NOT NEED TO CHANGE ANYTHING BELOW THIS LINE TO RUN THIS EXAMPLE
+  ///////////////////////////////////////////////////////////////////////////////////
+
+  const raw = JSON.stringify({
+      "user_app_id": {
+          "user_id": USER_ID,
+          "app_id": APP_ID
+      },
+      "inputs": [
+          {
+              "data": {
+                  "image": {
+                      "url": IMAGE_URL
+                  }
+              }
+          }
+      ]
+  });
+
+  const requestOptions = {
+      method: 'POST',
+      headers: {
+          'Accept': 'application/json',
+          'Authorization': 'Key ' + PAT
+      },
+      body: raw
+  };
+
+  // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
+  // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
+  // this will default to the latest version_id
+
+  fetch("https://api.clarifai.com/v2/models/" + MODEL_ID  + "/outputs", requestOptions)
+      .then(response => response.json())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+}
+
+
+
+
+ 
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state ={
+      input:'',
+      imageUrl: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    this.setState({input: event.target.value});
+  }
+
+  onButtonSubmit = () => {
+ Clarify(this.state.input);
+  }
+
+ render(){
+  return (
+    <div className="App">
+      <ParticlesBg type="cobweb" bg={true} />
+      <Navigation />
+    
+      <Logo />
+      <Rank />
+      <ImageLinkForm onInputChange={this.onInputChange} 
+      onButtonSubmit={this.onButtonSubmit}/>
+      <FaceRecognition imageUrl = {this.state.imageUrl} />
+      
+    </div>
+  );
+ }
+}
+
+export default App;
