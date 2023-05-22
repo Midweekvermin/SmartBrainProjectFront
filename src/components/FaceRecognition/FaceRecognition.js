c8a078b5b64064a06c65ba32e205b7aabcbcd6d6
Midweekvermin <113259156+Midweekvermin@users.noreply.github.com>
2023-05-22 16:00:11 -0600

    Add files via upload

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,12 +1,23 @@
 import React from "react";
-import './FaceRecognition.css';
+import "./FaceRecognition.css";
 
-const FaceRecognition = ({imageUrl}) => {
-    return (
-<div >
- <img alt='' src={imageUrl} height={500} width={500}/>
-</div>
-    );
-}
+const FaceRecognition = ({ imageUrl, box }) => {
+  return (
+    <div className="center ma mt3">
+      <div className="image-container">
+        <img alt="" id="inputimage" src={imageUrl} height={500} width={500} />
+        <div
+          className="bounding-box"
+          style={{
+            top: box.topRow,
+            right: box.rightCol,
+            bottom: box.bottomRow,
+            left: box.leftCol
+          }}
+        ></div>
+      </div>
+    </div>
+  );
+};
 
 export default FaceRecognition;
